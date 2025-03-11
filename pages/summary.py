fa3bafddcd079d1e4740798085f432f8692efb78
andyjjyuzmq <andyjjyuzmq@gmail.com>
2025-03-11 09:59:02 +0800

    update

--- pages/summary.py
+++ pages/summary.py
@@ -18,7 +18,11 @@ import pandas as pd
 
 st.title("HKIBIM_BIM_Automation_Arena_2025 - Andy")
 st.subheader("Summary")
+video_expander=st.expander("Video") 
+with video_expander:
+    st.video(r"image/final_video/HKIBIM_AUTOMATION_MASTER_CHALLENGE_VIDEO_FINAL.mp4")
 st.divider()
+
 
 col1,col2,col3=st.columns([1,1,1])
 with col1:
@@ -233,30 +237,29 @@ with ai_expander3:
             max_iterations=1
         )
         question = input_text
-        response = agent.run(question)
+        response = agent.invoke({"input": question})
         
 
-        sys.stdout = old_stdout
+        # sys.stdout = old_stdout
         verbose_output = captured_output.getvalue()
+        # print(verbose_output)
 
 
-        print("--------------------------------")
-        print(verbose_output)
-        verbose_output=verbose_output.split("Observation[0m")[1].split("[36;1m[1;3mNameError:")[0]
-        st.write(verbose_output)
-        print("--------------------------------")
-    
-    
-    
-    
-    
+        output_lines = verbose_output.split("\n")
+        thought = ""
+        observation = ""
+        for line in output_lines:
+            if "Thought:" in line:
+                thought = line.split("Thought:")[1].strip()
+            # elif "Observation" in line:
+            #     observation = line.split("Observation")[1].strip()
 
-
-
+        st.write(thought)
+        observation=verbose_output.split("Observation[0m")[1].split("[36;1m[1;3mNameError:")[0]
+        st.write(observation)
+        # st.write("aaa")
         
-
-
-
+        
 print(dfTransoms["length"].sum())
 
 
